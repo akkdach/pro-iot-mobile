d5284a44e4e7cc7ca72db08732580ef99468b362
TaWSaK <132742858+TaWSaK@users.noreply.github.com>
2025-12-24 09:39:12 +0700

    upload image V.4

--- src/Services/callUploadImage.tsx
+++ src/Services/callUploadImage.tsx
@@ -16,14 +16,18 @@ export default async function callUploadImage(params: UploadImageParams) {
     const data = res.data.dataResult;
     console.log("data Result Img info : ", data);
 
-    const { year, month } = splitDate(new Date(data.productioN_START_DATE));
+    const { year, month } = splitDate(new Date(data[0].productioN_START_DATE));
 
     const now = new Date();
+
+    console.log("data Result productioN_START_DATE : ", data[0].productioN_START_DATE);
+    console.log("data Result Year : ", year);
+    console.log("data Result Month : ", month);
 
     const form = new FormData();
     form.append("OrderId", params.orderId);
     form.append("TradCode", "refurbish");
-    form.append("OrderType", data.ordeR_TYPE || "01");
+    form.append("OrderType", data[0].ordeR_TYPE || "01");
     form.append("Year", year || now.getFullYear().toString());
     form.append("Month", month || (now.getMonth() + 1).toString().padStart(2, '0'));
     form.append("Image", params.image);
